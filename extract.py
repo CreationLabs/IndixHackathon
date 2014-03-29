f8a1818a3ca81e9b690685e2cdcdaec9323749aa
Akshay Dixit <akshaydixi@gmail.com>
2014-03-29 18:33:39 +0530

    url and result added to json dumper

--- extract.py
+++ extract.py
@@ -1,22 +1,25 @@
 import csv
-import json
+import simplejson as json
 from sklearn import svm 
 __DEBUG__ = False # False if production
 MAIN_JSON = {}
-#outfile = open('dump.json','w')
+outfile = open('dump.json','w')
 
-def getJson(row):
+def getJson(row,PorL,URL):
   row = row[2]
+  url = URL
   index = row.index('.com') + 4
   host = row[:index]
 
   if __DEBUG__:
     print 'URL : ', row
     print 'Host : ',host
+  
   others = row[index:].split('/') #Supposing that / means sub url divisions
   last = others[-1]
   tags = others[:-1]
   key = last.split('?')[0]
+  
   if __DEBUG__:
     print "Others : ", others
     print "Last : ",last
@@ -43,24 +46,29 @@ def getJson(row):
   else:
     queryparams = {}
   mainJson = {}
-  mainJson[host] = {}
-  mainJson[host][key] = queries
-  mainJson[host]["tags"] = tags
-  print mainJson
+  mainJson[url] = {}
+  mainJson[url]["host"] = host
+  mainJson[url]["key"] = queries
+  mainJson[url]["tags"] = tags
+  mainJson[url]["result"] = PorL
   return mainJson
   
-def addToDB(json):
-  MAIN_JSON.append(json)
+def addToDB(x,json):
+  MAIN_JSON[x] = (json)
 
 
 with open('indexFile.tsv','rb') as tsvin:
   tsvin = csv.reader(tsvin, delimiter='\t')
   line = 0
   for row in tsvin:
-      json = getJson(row)    
+      PorL = row[1]
+      ajson = getJson(row,PorL,row[2])    
+      addToDB(row[0],ajson)
       line+=1
       #raw_input() # Line by line input prompt
-  print MAIN_JSON
+  json.dump(MAIN_JSON,outfile,sort_keys=True,indent=4)
+  outfile.close()
+  print "DONE"
 
 
 
