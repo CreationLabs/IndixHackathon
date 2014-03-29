ef850af40c1e2880bad911353ff648374216e5b4
Akshay Dixit <akshaydixi@gmail.com>
2014-03-29 16:41:38 +0530

    extract.py

--- extract.py
+++ extract.py
@@ -5,7 +5,17 @@ def getJson(row):
   index = row.find('.com') + 4
   host = row[:index]
   others = row[index:].split('/') #Supposing that / means sub url divisions
-
+  last = others[-1]
+  beforeLast = '/'.join(others[:-1])
+  queryparams = last.split('?')[-1]
+  if len(queryparams):
+    queryparams.split('&')
+    queries = [{a[0] : a[1]} for a.split('=') in queryparams]
+  mainJson = {}
+  mainJson[host][beforeLast] = queryParams
+  print mainJson
+  return mainJson
+  
 
 
 with open('indexFile.tsv','rb') as tsvin:
