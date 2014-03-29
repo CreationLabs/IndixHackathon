8731f3a5a6b1808bcf3069df64b573d3122718e3
Akshay Dixit <akshaydixi@gmail.com>
2014-03-29 16:34:17 +0530

    extract written

--- extract.py
+++ extract.py
@@ -1,5 +1,23 @@
 import csv
+
+
+def getJson(row):
+  index = row.find('.com') + 4
+  host = row[:index]
+  others = row[index:].split('/') #Supposing that / means sub url divisions
+
+
+
 with open('indexFile.tsv','rb') as tsvin:
   tsvin = csv.reader(tsvin, delimiter='\t')
+  line = 0
   for row in tsvin:
-    
+    try:
+      json = getJson(row)
+      addToDB(json)
+      line+=1
+    except:
+      print 'Error'
+
+
+
